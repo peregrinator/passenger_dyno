ca870697d95c7fab188760783063a2890cf6f41b
Bob Burbach <bob.burbach@gmail.com>
2010-04-11 01:12:48 -0700

    Fix myriad of simple errors. Sort threads after grep so that pids match. Do a better grep for number of passenger threads for an app. And seperate total passenger memory from passenger memory for a server.

--- lib/dyno_check.rb
+++ lib/dyno_check.rb
@@ -13,8 +13,8 @@ class DynoCheck
     MongoMapper.database   = database_config['database']
     
     @servers                       = dyno_config['servers']
-    @passenger_memory_stats_output = `passenger-memory-stats`
-    @passenger_status_output       = `passenger-status`
+    @passenger_memory_stats_output = `sudo passenger-memory-stats`
+    @passenger_status_output       = `sudo passenger-status`
   end
   
   def run
@@ -43,36 +43,49 @@ class DynoCheck
     # Line 4 Total memory used by Nginx worker processes
     # Line 5 Total Passenger rails processes
     # Line 6 Total memory used by Passenger rails processes
-    overview_stats = `grep '###' "#{@passenger_memory_stats_output}" | awk '{print $3"\t"$6}'`.split("\n")
+    overview_stats = `echo "#{@passenger_memory_stats_output}" | grep '###' | awk '{print $3"\t"$6}'`.split("\n")
     
-    apache_workers    = overview_stats[0].split("\t")[0]
-    apache_memory     = overview_stats[1].split("\t")[1]
-    passenger_process = overview_stats[4].split("\t")[0]
-    passenger_memory  = overview_stats[5].split("\t")[1]
+    apache_workers_count = overview_stats[0].split("\t")[0]
+    apache_memory_used   = overview_stats[1].split("\t")[1]
     
-    @dyno_server_check.overview = PassengerOverview.new(:total_apache_processes    => apache_workers,
-                                                        :total_apache_memory       => apache_memory,
-                                                        :total_passenger_processes => passenger_process,
-                                                        :total_passenger_memory    => passenger_memory)
+    # here we really only want the passenger processes for our server (minus application and spawn server)
+    passenger_process_count = `echo "#{@passenger_memory_stats_output}" | grep 'Rails:' | grep 'govpulse'`.split("\n").length
+
+    passenger_memory_used   = overview_stats[5].split("\t")[1]
+    
+    # here we really only want the total mem for the threads for our server
+    memory_threads = `echo "#{@passenger_memory_stats_output}" | grep 'MB.*Rails' | awk '{ print $1"\t"$4"\t"$7 }' | sort`.split("\n")
+    passenger_memory_used_for_server = []
+    memory_threads.length.times do |i|
+      memory_thread = memory_threads[i].split("\t")
+      passenger_memory_used_for_server = passenger_memory_used_for_server + memory_thread[1]
+    end
+    
+    
+    @dyno_server_check.passenger_overview = PassengerOverview.new(:total_apache_processes            => apache_workers_count,
+                                                                  :total_apache_memory               => apache_memory_used,
+                                                                  :total_passenger_processes         => passenger_process_count,
+                                                                  :total_passenger_memory            => passenger_memory_used,
+                                                                  :total_passenger_memory_for_server => passenger_memory_used_for_server)
   end
   
   def thread_check
     # prints PID PrivateMem ProcessName
-    memory_threads = `grep 'MB.*Rails' "#{@passenger_memory_stats_output}" | awk '{ print $1"\t"$4"\t"$7 }'`.split('\n')
+    memory_threads = `echo "#{@passenger_memory_stats_output}" | grep 'MB.*Rails' | awk '{ print $1"\t"$4"\t"$7 }' | sort`.split("\n")
     
     # prints PID Sessions Processed Uptime(min) Uptime(sec) seperated by tabs
-    status_threads = `grep 'PID: ' "#{@passenger_status_output}" | awk '{ print $2"\t"$4"\t"6"\t"$8"\t"$9 }'`.split('\n')
+    status_threads = `echo "#{@passenger_status_output}" | grep 'PID: ' | awk '{ print $2"\t"$4"\t"$6"\t"$8"\t"$9 }' | sort`.split("\n")
     
     memory_threads.length.times do |i|
-      memory_thread = memory_threads[i-1].split("\t")
+      memory_thread = memory_threads[i].split("\t")
       pid           = memory_thread[0]
       private_mem   = memory_thread[1]
       process_name  = memory_thread[2]
       
-      status_thread      = status_threads[i-1].split("\t")
+      status_thread      = status_threads[i].split("\t")
       sessions_waiting   = status_thread[1]
       sessions_processed = status_thread[2]
-      uptime             = (status_thread[3] * 60) + status_thread[4]
+      uptime             = (status_thread[3].slice(0...-1) * 60) + status_thread[4].slice(0...-1)
       
       @dyno_server_check.passenger_threads << PassengerThread.new(:pid                => pid,
                                                                   :private_mem        => private_mem,
--- lib/passenger/passenger_overview.rb
+++ lib/passenger/passenger_overview.rb
@@ -4,6 +4,7 @@ class PassengerOverview < MongoStore
   key :total_apache_processes, Integer
   key :total_apache_memory,    Integer
   
-  key :total_passenger_processes, Integer
-  key :total_passenger_memory,    Integer 
+  key :total_passenger_processes,        Integer
+  key :total_passenger_memory,           Integer
+  key :passenger_memory_used_for_server, Integer 
 end
